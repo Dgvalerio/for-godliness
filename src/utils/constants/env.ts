85ab538101c51b47e160ab06ce9b9f0046d88c49
davi.valerio <al.davigvalerio@gmail.com>
2024-07-27 14:07:16 -0300

    fix(env): remove github environment

--- src/utils/constants/env.ts
+++ src/utils/constants/env.ts
@@ -6,8 +6,6 @@ export const env = createEnv({
   server: {
     NEXTAUTH_SECRET: z.string().min(1),
     NEXTAUTH_URL: z.string().url(),
-    GITHUB_ID: z.string().min(1),
-    GITHUB_SECRET: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_FIREBASE_API_KEY: z.string().min(1),
@@ -19,8 +17,6 @@ export const env = createEnv({
     // server
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    GITHUB_ID: process.env.GITHUB_ID,
-    GITHUB_SECRET: process.env.GITHUB_SECRET,
     // client
     NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID:
