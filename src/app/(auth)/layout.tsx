baadeafddf85e7f0633796235377c2c3fb9ebe37
davi.valerio <al.davigvalerio@gmail.com>
2024-07-27 17:54:27 -0300

    refactor(auth): improve sign-in in mobile screens

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -19,20 +19,20 @@ const PublicLayout: NextPage<PublicLayoutProps> = async ({ children }) => {
   return (
     <main
       className={cn(
-        'm-auto flex max-h-[80vh] w-full max-w-[80%] flex-1',
-        'rounded border border-zinc-700'
+        'm-auto flex w-full flex-1 sm:max-h-[80vh] sm:max-w-[80%]',
+        'border border-zinc-700 sm:rounded'
       )}
     >
-      <aside className="flex flex-[2] flex-col rounded-l bg-zinc-100 bg-auth bg-cover bg-bottom p-4 dark:bg-zinc-900">
+      <aside className="flex flex-[1] flex-col bg-zinc-100 bg-auth bg-cover bg-bottom p-4 dark:bg-zinc-900 sm:flex-[2] sm:rounded-l">
         <header className="flex justify-end">
           <ThemeToggleButton />
         </header>
       </aside>
-      <section className="flex flex-[3] flex-col items-center gap-4 rounded-r p-4">
+      <section className="flex flex-[4] flex-col items-center gap-4 rounded-r p-4 sm:flex-[3]">
         <span className="font-black uppercase text-zinc-900/60 transition hover:text-opacity-100 dark:text-zinc-100/60">
           CCB
         </span>
-        <div className="m-auto flex w-full max-w-[60%] flex-col">
+        <div className="m-auto flex w-full flex-col sm:max-w-[60%]">
           {children}
         </div>
         <div className="h-6" />
